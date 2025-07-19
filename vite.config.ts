5c7e540ffbca4138d4e63ec6aaf1a6c3683be646
mavlekiev <mavlekiev@gmail.com>
2025-07-20 02:49:19 +0300

    feat: add vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests-utils/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      include: ['src//*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src//.test.{js,jsx,ts,tsx}',
+        'src/**/.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/*/.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 });
