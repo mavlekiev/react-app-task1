5f273753f22834f39910a75041199a2afb0b808c
mavlekiev <mavlekiev@gmail.com>
2025-08-10 20:37:15 +0300

    init: start task

--- src/test-utils/context/ThemeContext.test.tsx
+++ src/test-utils/context/ThemeContext.test.tsx
@@ -56,10 +56,10 @@ describe('ThemeContext and ThemeProvider', () => {
     const toggleButton = screen.getByRole('button', { name: /Toggle Theme/i });
     const themeSpan = screen.getByTestId('theme');
 
-    fireEvent.click(toggleButton); 
+    fireEvent.click(toggleButton);
     expect(themeSpan).toHaveTextContent('dark');
 
-    fireEvent.click(toggleButton); 
+    fireEvent.click(toggleButton);
     expect(themeSpan).toHaveTextContent('light');
   });
 
