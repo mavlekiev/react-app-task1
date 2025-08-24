565b6c12ba64db5566e5d901dbd5471a0037ea93
mavlekiev <mavlekiev@gmail.com>
2025-08-24 17:28:58 +0300

    feat: add Tailwind

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,17 +1,13 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-// import "./MainPage.css";
-
 const MainPage = () => {
-  const handleClick = () => {
-    
-  }
+  const handleClick = () => {};
   return (
     <div className="main-container">
       <h1 className="text-3xl font-bold">Forms</h1>
       <div className="main-container__buttons ">
-        <button className="bg-green-500 text-white px-4 py-2 rounded" onClick={handleClick}>
+        <button
+          className="bg-green-500 text-white px-4 py-2 rounded"
+          onClick={handleClick}
+        >
           Click to open the form
         </button>
         <button className="button" onClick={handleClick}>
