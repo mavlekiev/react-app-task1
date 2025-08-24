7be028b5fe4e63aad4bd541f81301b2bc2770066
mavlekiev <mavlekiev@gmail.com>
2025-08-24 17:25:36 +0300

    feat: add Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
+import "./index.css";
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import { Provider } from "react-redux";
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import "./MainPage.scss";
+// import "./MainPage.css";
 
 const MainPage = () => {
+  const handleClick = () => {
+    
+  }
   return (
     <div className="main-container">
-      <h1 className="main-container__title">Forms</h1>
+      <h1 className="text-3xl font-bold">Forms</h1>
+      <div className="main-container__buttons ">
+        <button className="bg-green-500 text-white px-4 py-2 rounded" onClick={handleClick}>
+          Click to open the form
+        </button>
+        <button className="button" onClick={handleClick}>
+          Click to open the form
+        </button>
+      </div>
     </div>
   );
 };
