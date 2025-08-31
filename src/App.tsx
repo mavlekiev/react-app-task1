9a1c8c7e74c11e48c8307963cba21159206142dc
mavlekiev <mavlekiev@gmail.com>
2025-08-31 15:40:41 +0300

    init: start task

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import NotFound from "./pages/NotFound/NotFound";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
